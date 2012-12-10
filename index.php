3d6d7a0fa8fcaa2b43e110f7008f65cd926e9337
Nicolas Vigier <boklm@mageia.org>
2012-12-10 17:07:54 +0000

    Move favicon.png to themes/mageia/favicon.png
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/web@6679 5b7c3c08-40e5-403b-9995-ace06908e4af

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ publish_stats_headers(
     <meta name="robots" content="<?php echo $robots; ?>">
     <link rel="home" href="<?php echo $g_root_url; ?>">
     <link rel="author" href="http://www.mageia.org/">
-    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="icon" type="image/png" href="themes/mageia/favicon.png">
     <link rel="stylesheet" href="themes/mageia/style.css">
     <meta name="viewport" content="width=900,initial-scale=1,user-scalable=yes">
 </head>
