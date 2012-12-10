7733120c3e31651454d952b821e5b4c6854b9788
Nicolas Vigier <boklm@mageia.org>
2012-12-10 17:07:55 +0000

    Create themes/mageia/page_footer.php
    git-svn-id: svn+ssh://svn.mageia.org/svn/soft/build_system/web@6680 5b7c3c08-40e5-403b-9995-ace06908e4af

--- index.php
+++ index.php
@@ -328,13 +328,6 @@ else
     </ul>
     <script src="js/jquery.js"></script>
     <script src="js/pkgsubmit.js"></script>
-    <script src="//nav.mageia.org/js/"></script>
-    <div class="clear"></div>
-    <hr />
-    <footer>
-        <p>Generated at <?php echo $date_gen; ?>.
-            Code for this page is in <a rel="nofollow" href="http://svnweb.mageia.org/soft/build_system/web/">http://svnweb.mageia.org/soft/build_system/web/</a>.</p>
-    </footer>
-    </article>
-</body>
-</html>
+<?php
+require __DIR__ . '/themes/mageia/page_footer.php';
+?>
---
+++ themes/mageia/page_footer.php
@@ -0,0 +1,10 @@
+    <script src="//nav.mageia.org/js/"></script>
+    <div class="clear"></div>
+    <hr />
+    <footer>
+        <p>Generated at <?php echo $date_gen; ?>.
+            Code for this page is in <a rel="nofollow" href="http://svnweb.mageia.org/soft/build_system/web/">http://svnweb.mageia.org/soft/build_system/web/</a>.</p>
+    </footer>
+    </article>
+</body>
+</html>
